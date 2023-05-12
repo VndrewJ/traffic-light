1841c59d6a3a84cc1a49ba731a233decc2f54bc4
VndrewJ <andyjkoh@gmail.com>
2023-05-12 16:29:22 +1200

    added pseudocode for finite state table

--- MECHENG_313_A2/MECHENG_313_A2/Tasks/FiniteStateMachine.cs
+++ MECHENG_313_A2/MECHENG_313_A2/Tasks/FiniteStateMachine.cs
@@ -7,6 +7,30 @@ namespace MECHENG_313_A2.Tasks
 {
     public class FiniteStateMachine : IFiniteStateMachine
     {
+        /*Pseudocode for table
+         * initialise delegates
+         * ????
+         * 
+         * initialise dictionary variables
+         * String currentState, nextState
+         * 
+         * initialise double-nested dict
+         * var FST = new Dictionary<current state, Dictionary<next state, action delegate>>();
+         * 
+         * Add current state entry
+         * FST.Add("State A", new Dictionary<next state, action delegate>());
+         * 
+         * Add next state + action entry in nested dictionary
+         * data["State A"].Add("State B", action);
+         * 
+         * Repeat for other states
+         * FST.Add("State B", new Dictionary<next state, action delegate>());
+         * data["State B"].Add("State C", action);
+         *
+         * repeat...
+         */
+
+
         public void AddAction(string state, string eventTrigger, TimestampedAction action)
         {
             // TODO: Implement this
